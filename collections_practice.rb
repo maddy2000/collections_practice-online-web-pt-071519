0a84d75cfd2f9d371ecdfb601a86960ff8efc810
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-12 20:27:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort do |array|
+    25 <=> 7
+end
+end
 
+def sort_array_desc(array)
+  array.sort.reverse 
+    
+    #array.sort do |array|
+    # 7 <=> 15
+   end
+ 
+  def sort_array_char_count(array)
+      array.sort {|a,b| a.length <=> b.length}
+    end
+
+def swap_elements (array)
+ array[1], array[2] = array[2], array[1]
+ return array
+end  
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array) 
+  array.each do |word|
+    word[2] = "$"
+end 
+end
+  
+ def find_a 
+  
+  
+  
